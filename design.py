a56d6930fdc97400f4be117387823fa3e5ecc207
dushyantrawat51-source <dushyantrawat51@gmail.com>
2026-09-07 16:57:51 +0530

    Update design.py

--- design.py
+++ design.py
@@ -42,17 +42,16 @@ def get_designs(cloth_type):
             {
                 "name": "Flower Pattern1",
                 "image":
-                "https:./designs/floral1.png"
+                "https://dushyantrawat51-source.github.io/cloth-designer/designs/floral1.png"
             },
             {
                 "name": "Flower Pattern2",
-                "image":
-                "https:./designs/floral2.png"
+                "image":"https://dushyantrawat51-source.github.io/cloth-designer/designs/floral2.png"
             },
             {
                 "name": "Flower Pattern3",
                 "image":
-                "https:./designs/floral3.png"
+                "https://dushyantrawat51-source.github.io/cloth-designer//designs/floral3.png"
             }
         ]
     }
